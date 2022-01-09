ebc1ada52397f07bcd2ca7969dbde9bf251a4b4e
vzakharchenko <vzakharchenko@modeln.com>
2022-01-10 01:40:23 +0200

    fix

--- server/index.ts
+++ server/index.ts
@@ -1,42 +1,43 @@
 import express from 'express';
-import  cors from 'cors';
-import  bodyParser from 'body-parser';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
 import {saveConfig, config, ConfigJson} from './lib/env';
 import {saveSmartThingDeviceInfo} from "./lib/smartthings/registerDevice";
 import {startApplication} from "./lib/smartthings/ServiceBackend";
 
 const corsOptions = {
-    origin(o:any, callback:any) {
-        callback(null, true);
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: true,
-    credentials: true,
-    maxAge: 3600,
+  origin(o:any, callback:any) {
+    callback(null, true);
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: true,
+  credentials: true,
+  maxAge: 3600,
 };
 
-config().then(async (currentConfig:ConfigJson)=>{
+config().then(async (currentConfig:ConfigJson) => {
+  const {port} = currentConfig.server;
 
+  const server = express();
+  server.use(bodyParser.json());
+  server.use(cors(corsOptions));
 
-    const { port } = currentConfig.server;
+  server.get('/health', (req, res) => {
+    const status = {status: 'OK'};
+    res.send(JSON.stringify(status));
+  });
 
-    const server = express();
-    server.use(bodyParser.json());
-    server.use(cors(corsOptions));
+  server.get('/registerDevice', async (req, res) => {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    await saveSmartThingDeviceInfo(req, res);
+  });
 
-    server.get('/health', (req, res) => {
-        const status = { status: 'OK' };
-        res.send(JSON.stringify(status));
-    });
+  server.listen(port, () => {
+    console.info(`HTTP intercom listening on port ${port}`);
+  });
 
-    server.get('/registerDevice', async (req, res) => {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        await saveSmartThingDeviceInfo(req, res);
-    });
-
-    server.listen(port, () => {
-        console.info(`HTTP intercom listening on port ${port}`);
-    });
-
-    await startApplication();
-})
+  await startApplication();
+}).catch((e:any) => {
+  throw new Error(`Configuration issue ${e}`);
+});
